39f5cc1663ad889445fe4a3a772ea1034f6872df
ABISHEK B <abishek0970@gmail.com>
2025-11-25 21:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
 
-function SportsCar(make, model, topSpeed) {}
+  function getMakeModel() {
+    return make + " " + model;
+  }
+
+  this.getMakeModel = getMakeModel;
+}
+
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model);
+
+  this.topSpeed = topSpeed;
+
+  function getTopSpeed() {
+    return topSpeed;
+  }
+
+  this.getTopSpeed = getTopSpeed;
+}
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+console.log(car.getMakeModel()); 
+
+console.log(car.getTopSpeed());
+
 
 // Do not change the code below
 window.Car = Car;
