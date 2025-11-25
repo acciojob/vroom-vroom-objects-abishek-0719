3b20989faca93658e748ed8d670f805df6274e0b
ABISHEK B <abishek0970@gmail.com>
2025-11-25 21:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,33 +2,42 @@
 function Car(make, model) {
   this.make = make;
   this.model = model;
-
-  function getMakeModel() {
-    return make + " " + model;
-  }
-
-  this.getMakeModel = getMakeModel;
 }
+
+// Method on Car prototype
+Car.prototype.getMakeModel = function () {
+  return this.make + " " + this.model;
+};
 
 function SportsCar(make, model, topSpeed) {
-  Car.call(this, make, model);
-
+  Car.call(this, make, model); 
   this.topSpeed = topSpeed;
-
-  function getTopSpeed() {
-    return topSpeed;
-  }
-
-  this.getTopSpeed = getTopSpeed;
 }
+
+// Inherit Car prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Fix constructor pointer
+SportsCar.prototype.constructor = SportsCar;
+
+// Method on SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
+
+// Do not change the code below
+window.Car = Car;
+window.SportsCar = SportsCar;
+
 
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 
 console.log(car.getMakeModel()); 
 
+
 console.log(car.getTopSpeed());
 
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+console.log(car instanceof Car);
+
+console.log(car instanceof SportsCar);
